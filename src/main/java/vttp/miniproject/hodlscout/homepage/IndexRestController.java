2f7275af525e89a58547d16e5c7c25b594fa6d9b
Seah Shun Zhou <seahsz@gmail.com>
2024-12-12 22:57:55 +0800

    Completed Login / SignUp Pages

---
+++ src/main/java/vttp/miniproject/hodlscout/homepage/IndexRestController.java
@@ -0,0 +1,8 @@
+package vttp.miniproject.hodlscout.homepage;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexRestController {
+    
+}
--- src/main/java/vttp/miniproject/hodlscout/login/SignUpController.java
+++ src/main/java/vttp/miniproject/hodlscout/login/SignUpController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -28,7 +29,7 @@ public class SignUpController {
     }
 
     @PostMapping
-    public String postSignUp(@Valid UserModel newUser, BindingResult binding) {
+    public String postSignUp(@Valid @ModelAttribute(name = "user") UserModel newUser, BindingResult binding) {
 
         if(binding.hasErrors())
             return "signup";
